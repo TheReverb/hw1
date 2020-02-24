78a4207aa4ff2d7251c91cabbfb28bbc8cf799c5
Jonah <jonahkohn@gmail.com>
2020-02-23 19:09:19 -0800

    Finalized results, added chart

--- cache_test.cpp
+++ cache_test.cpp
@@ -11,12 +11,12 @@ using namespace chrono;
 
 
 struct node {
-	int buffer;
+	uint64_t buffer;
 	node* next = nullptr;
 };
 
 
-double benchmark(int N, int iters) {
+double benchmark(int N, int reps) {
 
 	vector<node> linked_list(N);
   vector<node*> list_order(N); //stores pointers to nodes in linked_list
@@ -30,42 +30,45 @@ double benchmark(int N, int iters) {
 	for (int i = 0; i < N-1; i++) {  //links nodes based on the order found in list_order
 		list_order[i]->next = list_order[i+1];
 	}
-	node* head = list_order[0]; //close the loop such that the linked_list points in a circle.
+	node* head = list_order[0];
 
 	auto start = chrono::steady_clock::now();
-	for (int i = 0; i < iters; i++) {
 
+	for (int i = 0; i < reps; i++) {
 		node* current = head;
+
 		while (current) {
 			current = current->next;
 		}
 	}
 
   auto end = chrono::steady_clock::now();
+
+
 	auto elapsed =  end - start;
 	auto ns = duration_cast<nanoseconds>(elapsed).count();
 
-	return ns / double(N * iters); //calculates average ns per operation
+	return ns / double(N * reps); //calculates average ns per operation
 }
 
 
 int main(){
-  cout << "# Bytes    Time(ns)" << endl;
+  cout << "# Kbytes    Time(ns)" << endl;
 
-	int start = 10; //iterates from 2^min to 2^max.
+	int start = 10; //iterates from 2^start to 2^stop.
 	int stop = 26;
 
 	for (int i = start; i <= stop; i++) {
 
 		int N = int(pow(2, double(i)));
-		int reps = int(2e10 / pow(N, 1.5)); //scales the repetitions inversely to the size of the list.
+		int reps = int(2e10 / pow(N, 1.5)); //scales the repetitions roughly inversely to the size of the list.
 
 		if (reps<1){ //makes sure to round up
       reps = 1;
     }
 
 		auto result = benchmark(N, reps);
-		cout << (N*sizeof(node)) << "   " << result << "   # (N=" << N << ", reps=" << reps << ") " << endl;
+		cout << ((N*sizeof(node))/1024) << "   " << result << "   # (N=" << N << ", reps=" << reps << ") " << endl;
 
   }
 }
